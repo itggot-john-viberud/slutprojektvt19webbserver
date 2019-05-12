db7ea7ee2407f3168f9ae2ce6aebcb2bec7b183f
itggot-john-viberud <john.viberud@itggot.se>
2019-05-12 13:18:34 +0200

    Uptadering

--- app.rb
+++ app.rb
@@ -6,35 +6,43 @@ require 'byebug'
 require 'json'
 require 'sinatra-websocket'
 require_relative 'function.rb'
-
 enable :sessions
-
+include module
 set :secured_paths, ["/the_dark_room/:username", "/new_message"]
 set :sockets, []
 set :server, 'thin'
 
-
+# Check if user is logged in and give thereafter acses to the sites
+#
+# @session [String] :user, the name of the user
 before do
     if settings.secured_paths.any? { |elem| request.path.start_with?(elem) } 
-        if session[:user]
-        else
-            halt 403
-        end
+        halt 403 unless session[:user]
     end
 end
 
+# Display error 403 when user doent have the authority
 error 403 do
     "Forbidden dude"
 end
 
+# Display error 404
 error 404 do
     "muuu"
 end
 
+# Display Landing Page
+#
 get("/") do
     slim(:index)
 end
 
+# Loggs in and redirect to (on sucses) '/the_dark_room/', on failed redirect to '/failed'
+#
+# @param [String] :Username, The username of the user
+# @param [String] :Password, The password of the user
+#
+# @see MyModel#loggin
 post("/login") do
     if loggin(params) == true
         session[:user] = params["Username"]
@@ -44,30 +52,68 @@ post("/login") do
     end
 end
 
+# Updates an existing logged in status to not logged in and redirects to '/'
+#
 post("/logout") do
     session.destroy
     redirect("/")
 end
 
+# Display failed Page
+#
 get("/failed") do
     slim(:failed)
 end
+
+# Display new page, where you can register a user
+#
 get("/new") do
     slim(:new)
 end
+
+# Creates a new user and redirects to '/'
+#
+# @param [String] :Username, The name of the new user
+# @param [String] :Password1, The first password of the new user
+# @param [String] :Password2, The secound password of the new user
+# @param [String] :Mail, The mail of the new user
+# @param [File] :file, The profile picture of the new user
+#
+# @see MyModel#create_user
 post("/create") do
-    create_user()
+    if create_user(params) == true
+        redirect('/')
+    else
+        redirect('/failed')
+    end
 end 
 
-
+# Attempts to create the user and redirects to ('/')
+#
+# @param [String] Username, The username
+# @param [String] Password1, The password
+# @param [String] Password2, The repeated password
+# @param [String] :Mail, The mail of the new user
+# @param [File] :file, The profile picture of the new user
+#
+# @see MyModel#create_user
 get("/the_dark_room/:username") do
     username = session[:user]
     room= chattrooms(username)
     chat = "NOOB"
+    user = "none"
     slim(:the_dark_room, locals:{
-        rooms: room, chats: chat})
+        rooms: room, chats: chat, users: user})
 end
 
+# Displays room page, based on user, chat id
+# A websocket, which creates a connection between computer and server
+#
+# @param [String] query_string, The search parameters delimited by spaces
+#
+# @see MyModel#chattrooms
+# @see MyModel#show
+# @see MyModel#send_message
 get('/the_dark_room/:username/:id') do
     room_id = params["id"]
     if params["id"] != "css.css"
@@ -75,11 +121,11 @@ get('/the_dark_room/:username/:id') do
     end
     username = session[:user]
     room = chattrooms(username)
-    chat = show(room_id)
+    chat, user = show(room_id)
     
     if !request.websocket?
         slim(:the_dark_room, locals:{
-            rooms: room, chats: chat})
+            rooms: room, chats: chat, users: user})
     else
         request.websocket do |ws|
             ws.onopen do
@@ -100,8 +146,6 @@ get('/the_dark_room/:username/:id') do
                         }.to_json
                     ) 
                 } }
-                # EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
-                # EM.next_tick { settings.sockets.each{|s| s.send(session[:user]) } }
             end
             ws.onclose do
                 warn("websocket closed")
@@ -111,41 +155,98 @@ get('/the_dark_room/:username/:id') do
     end
 end
 
-post("/new_message") do
-    send_message(params)
+# Attempts post picture and updates the session
+#
+# @param [File] file, the picture file
+#
+# @see MyModel#send_picture
+post("/new_picture") do
+    send_picture(params)
     redirect("/the_dark_room/:username/#{session[:room_id]}")
 end 
 
+# Deletes an existing message and updates the session
+#
+# @param [Integer] :id, The ID of the message
+#
+# @see MyModel#delete
 post('/delete/:id') do
     delete(params)
     redirect("/the_dark_room/:username/#{session[:room_id]}")
 end
 
+# Display edit page based on mesage id
+#   
+# @param [Integer] :id, The ID of the message
+#
+# @see MyModel#edit
 get("/edit/:id") do
     result = edit(params)
     slim(:edit, locals:{
     chat: result.first})    
 end
 
+# Edits an existing message and updates session
+#
+# @param [Integer] :id, The ID of the message
+# @param [String] Text, The text of the message
+# @param [File] Bild, The picture of the message
+#
+# @see MyModel#edit_execute
 post('/edit_execute/:id') do
     edit_execute(params)
     redirect("/the_dark_room/:username/#{session[:room_id]}")
 end
 
+# Display editprofil page based on user
+#   
+# @param [String] :user, The username of the user
+#
+# @see MyModel#editprofil
+get("/editprofil/:user") do
+    result = editprofil(params)
+    slim(:editprofil, locals:{
+    profil: result.first})    
+end
+
+# Edits an existing user and redirects to '/the_dark_room'
+#
+# @param [String] :user, The username of the user
+# @param [String] Text, The text of the message
+# @param [File] Bild, The picture of the message
+#
+# @see MyModel#editprofil_execute
+post('/editprofil_execute/:user') do
+    session[:user] = editprofil_execute(params)
+    redirect("/the_dark_room/:username")
+end
+
+# Display create room Page
+#
 get("/create_room") do
     slim(:create_room)
 end
 
+# Creates a new room and redirects to '/the_dark_room'
+#
+# @param [String] Roomname, The title of the new room
+#
+# @see MyModel#finish_room
 post("/finish_room") do
     finish_room(params)
     redirect("/the_dark_room/:username")
 end
 
 get("/invite/:id") do
-    byebug
     slim(:invite, locals:{info_invite: params})  
 end
 
 post("/invite_execute/:id") do
     invite_execute(params)
+end
+
+
+post("/leave/:room_id") do
+    leave(params)
+    redirect("/the_dark_room/:username")
 end
--- function.rb
+++ function.rb
@@ -6,182 +6,237 @@ require 'byebug'
 require_relative 'app.rb'
 enable :sessions
 
-
-# require 'sinatra-websocket'
-
-
-
-
-
-def connect()
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
-    return db
-end
-
-def create_user()
-    db = connect()
-    new_name = params["Username"]
-    new_password = params["Password1"]
-    new_mail = params["Mail"]
+module MyModule
     
-    if params["Password1"] == params["Password2"]
-        new_password_hash = BCrypt::Password.create(new_password)
-        db.execute("INSERT INTO user (Username, Password, Mail) VALUES (?,?,?)", new_name, new_password_hash, new_mail)
-        redirect("/")
-    else 
-        redirect("/failed")
-    end
-end
-
-def loggin(params)
-    db = connect()
-    result = db.execute("SELECT Username, Password FROM user WHERE Username = '#{params["Username"]}'")
-    if BCrypt::Password.new(result[0]["Password"]) == params["Password"]
-        return true
-    else
-        return false
-    end
-end
-
-def get_user_id(username)
-    db = connect()
-    user_id = db.execute("SELECT Id FROM user WHERE Username = ?", username)
-    return user_id.first
-end
 
 
-def get_rooms_in_room(user_id)
-    db = connect()
-    rooms = db.execute("SELECT room.Id, room.Name, user_room.Role from room
-        inner join user_room on room.Id = user_room.RoomId
-        where user_room.UserId = ?", user_id["Id"])
-        return rooms
-end
-    
-def chattrooms(username)
-    db = connect()
-    user_id = get_user_id(username)
-    roominfo = get_rooms_in_room(user_id)
-    return roominfo
-end
-
-def get_chat(room_id)
-    db = connect()
-    
-    result = db.execute("SELECT Id, Username, Text, Bild FROM chat WHERE RoomId = ?", room_id.to_i)
-
-    return result
-end
-
-
-def show(room_id)
-    chat = get_chat(room_id)
-
-    return chat
-    
-end
-
-def picture(file, filename)
-    file_name = SecureRandom.hex(10)
-    oof = filename.split('.')
-    file_name <<'.'
-    file_name << oof[1]
-    File.open("./public/img/chat/#{file_name}", 'wb') do |f|
-        f.write(file.read)
-    end
-    return file_name
-end
-
-#FileUtils.cp( params["file"]["tempfile"].path, "./omg.jpg")
-
-def send_message(params, msg)
-    db = connect()
-    new_text = msg
-    creator = session[:user]
-    room_id = session[:room_id]
-    if params[:file]
-        filename = params[:file][:filename]
-        file = params[:file][:tempfile]
-        file_name = picture(file, filename)
-        result = db.execute("INSERT INTO chat (RoomId, Text, Bild, Username) VALUES (?,?,?,?)", room_id, new_text, file_name, creator)
-    else
-        result = db.execute("INSERT INTO chat (RoomId, Text, Username) VALUES (?,?,?)", room_id, new_text, creator)
+    def connect()
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
+        return db
     end
 
-    result = db.execute("SELECT Id FROM chat WHERE Text = ? AND Username = ?", new_text, creator)
-    return result[-1].first[1]
-end
-
-def delete(params)
-    db = connect()
-    id = params["id"]
-    result_new = db.execute("DELETE FROM chat WHERE Id=?", id)
-end
-
-def edit(params)
-    db = connect()
-    id = params["id"]
-    result = db.execute("SELECT RoomId, Text, Bild, Id, Username FROM chat WHERE Id=?", id)
-    return result
-end
-
-def edit_execute(params)
-    db = connect()
-    new_text = params["Text"]
-    id = params["id"]
-
-    if params[:file]
-        filename = params[:file][:filename]
-        file = params[:file][:tempfile]
-        file_name = picture(file, filename)
-        result_new = db.execute("UPDATE chat
-            SET Text = ?, Bild = ?
-            WHERE Id = ?",
-            new_text, file_name, id)
-    else
-        result_new = db.execute("UPDATE chat
-            SET Text = ?
-            WHERE Id = ?",
-            new_text, id)
+    def picture_profil(file, filename)
+        file_name = SecureRandom.hex(10)
+        oof = filename.split('.')
+        file_name <<'.'
+        file_name << oof[1]
+        File.open("./public/img/profil_img/#{file_name}", 'wb') do |f|
+            f.write(file.read)
+        end
+        return file_name
     end
-end
-    
-def get_room_id(roomname)
-    db = connect()
-    room_id = db.execute("SELECT Id FROM room WHERE Name = ?", roomname)
-    return room_id.first["Id"]
-end
 
-def finish_room(params)
-    db = connect()
-    new_name = params["Roomname"]
-    if params["Password1"]
+    def create_user()
+        db = connect()
+        new_name = params["Username"]
         new_password = params["Password1"]
+        new_mail = params["Mail"]
+        
         if params["Password1"] == params["Password2"]
             new_password_hash = BCrypt::Password.create(new_password)
-            db.execute("INSERT INTO room (Name, Type, Password) VALUES (?,?,?)", new_name, 2, new_password_hash)
+            if params[:file]
+                filename = params[:file][:filename]
+                file = params[:file][:tempfile]
+                file_name = picture_profil(file, filename)
+                result = db.execute("INSERT INTO user (Username, Password, Mail, Bild) VALUES (?,?,?,?)", new_name, new_password_hash, new_mail, file_name)
+            else
+                db.execute("INSERT INTO user (Username, Password, Mail) VALUES (?,?,?)", new_name, new_password_hash, new_mail)
+            end
+            return true
         else 
-            redirect("/failed")
+            return false
         end
-    else
-        db.execute("INSERT INTO room (Name, Type) VALUES (?,?)", new_name, 1)
-
     end
-    user_id = get_user_id(session[:user])
-    room_id = get_room_id(new_name)
-    db.execute("INSERT INTO user_room (UserId, RoomId, Role) VALUES (?,?,?)", user_id, room_id, "Admin")
-end
+
+    def loggin(params)
+        db = connect()
+        result = db.execute("SELECT Username, Password FROM user WHERE Username = '#{params["Username"]}'")
+        if BCrypt::Password.new(result[0]["Password"]) == params["Password"]
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_user_id(username)
+        db = connect()
+        user_id = db.execute("SELECT Id FROM user WHERE Username = ?", username)
+        return user_id.first
+    end
 
 
-def invite_execute(params)
-    db = connect()
-    user_id = db.execute("SELECT Id FROM user WHERE Username = ?", params["username"])
-    if user_id.length > 0
-        byebug
-        db.execute("INSERT INTO user_room (UserId, RoomId, Role) VALUES (?,?,?)", user_id.first["Id"], params["id"], params["role"])
-        redirect("/the_dark_room/:username")
-    else
-        redirect("/invite/:id")
-    end  
+    def get_rooms_in_room(user_id)
+        db = connect()
+        rooms = db.execute("SELECT room.Id, room.Name, user_room.Role from room
+            inner join user_room on room.Id = user_room.RoomId
+            where user_room.UserId = ?", user_id["Id"])
+            return rooms
+    end
+        
+    def chattrooms(username)
+        db = connect()
+        user_id = get_user_id(username)
+        roominfo = get_rooms_in_room(user_id)
+        return roominfo
+    end
+
+    def get_chat(room_id)
+        db = connect()
+        
+        chat = db.execute("SELECT Id, Username, Text, Bild FROM chat WHERE RoomId = ?", room_id.to_i)
+        users = db.execute("SELECT user.Id, user.Username, user.Bild, user_room.Role from user
+            inner join user_room on user.Id = user_room.UserId
+            where user_room.RoomId = ?", room_id.to_i)
+
+        return chat, users
+    end
+
+
+    def show(room_id)
+        chat = get_chat(room_id)
+
+        return chat
+        
+    end
+
+    def picture(file, filename)
+        file_name = SecureRandom.hex(10)
+        oof = filename.split('.')
+        file_name <<'.'
+        file_name << oof[1]
+        File.open("./public/img/chat/#{file_name}", 'wb') do |f|
+            f.write(file.read)
+        end
+        return file_name
+    end
+
+    #FileUtils.cp( params["file"]["tempfile"].path, "./omg.jpg")
+    def send_picture(params)
+        db = connect()
+        creator = session[:user]
+        room_id = session[:room_id]
+        if params[:file]
+            filename = params[:file][:filename]
+            file = params[:file][:tempfile]
+            file_name = picture(file, filename)
+            result = db.execute("INSERT INTO chat (RoomId, Bild, Username) VALUES (?,?,?)", room_id, file_name, creator)
+        end
+    end
+
+    def send_message(params, msg)
+        db = connect()
+        new_text = msg
+        creator = session[:user]
+        room_id = session[:room_id]
+        if params[:file]
+            filename = params[:file][:filename]
+            file = params[:file][:tempfile]
+            file_name = picture(file, filename)
+            result = db.execute("INSERT INTO chat (RoomId, Text, Bild, Username) VALUES (?,?,?,?)", room_id, new_text, file_name, creator)
+        else
+            result = db.execute("INSERT INTO chat (RoomId, Text, Username) VALUES (?,?,?)", room_id, new_text, creator)
+        end
+
+        result = db.execute("SELECT Id FROM chat WHERE Text = ? AND Username = ?", new_text, creator)
+        return result[-1].first[1]
+    end
+
+    def delete(params)
+        db = connect()
+        id = params["id"]
+        result_new = db.execute("DELETE FROM chat WHERE Id=?", id)
+    end
+
+    def edit(params)
+        db = connect()
+        id = params["id"]
+        result = db.execute("SELECT RoomId, Text, Bild, Id, Username FROM chat WHERE Id=?", id)
+        return result
+    end
+
+    def edit_execute(params)
+        db = connect()
+        new_text = params["Text"]
+        id = params["id"]
+
+        if params[:file]
+            filename = params[:file][:filename]
+            file = params[:file][:tempfile]
+            file_name = picture(file, filename)
+            result_new = db.execute("UPDATE chat
+                SET Text = ?, Bild = ?
+                WHERE Id = ?",
+                new_text, file_name, id)
+        else
+            result_new = db.execute("UPDATE chat
+                SET Text = ?
+                WHERE Id = ?",
+                new_text, id)
+        end
+    end
+
+    def editprofil(params)
+        db = connect()
+        user = session[:user]
+        result = db.execute("SELECT Username, Bild, Id FROM user WHERE Username=?", user)
+        return result
+    end
+
+    def editprofil_execute(params)
+        db = connect()
+        new_name = params["username"]
+        id = get_user_id(session[:user])
+        if params[:file]
+            filename = params[:file][:filename]
+            file = params[:file][:tempfile]
+            file_name = picture_profil(file, filename)
+            byebug
+            result_new = db.execute("UPDATE user
+                SET Username = ?, Bild = ?
+                WHERE Id = ?",
+                new_name, file_name, id["Id"])
+        else
+            result_new = db.execute("UPDATE user
+                SET Username = ?
+                WHERE Id = ?",
+                new_name, id["Id"])
+        end
+        return new_name
+    end
+
+    def get_room_id(roomname)
+        db = connect()
+        room_id = db.execute("SELECT Id FROM room WHERE Name = ?", roomname)
+        return room_id.first["Id"]
+    end
+
+    def finish_room(params)
+        db = connect()
+        new_name = params["Roomname"]
+        
+        db.execute("INSERT INTO room (Name) VALUES (?)", new_name)
+
+        user_id = get_user_id(session[:user])
+        room_id = get_room_id(new_name)
+        db.execute("INSERT INTO user_room (UserId, RoomId, Role) VALUES (?,?,?)", user_id["Id"], room_id, "Admin")
+    end
+
+
+    def invite_execute(params)
+        db = connect()
+        user_id = db.execute("SELECT Id FROM user WHERE Username = ?", params["username"])
+        if user_id.length > 0
+            db.execute("INSERT INTO user_room (UserId, RoomId, Role) VALUES (?,?,?)", user_id.first["Id"], params["id"], params["role"])
+            redirect("/the_dark_room/:username")
+        else
+            redirect("/invite/:id")
+        end  
+    end
+
+    def leave(params)
+        db = connect()
+        user_id = get_user_id(session[:user])
+        db.execute("DELETE FROM user_room WHERE UserId = ? AND RoomId = ?", user_id["Id"], params["room_id"])
+    end
 end
